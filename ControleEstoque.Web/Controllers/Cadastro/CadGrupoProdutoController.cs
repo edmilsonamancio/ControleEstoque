da75168f3d486f2ad2c6d1da154acb35552b7f74
Edmilson Amancio <edmilson.aamancio@gmail.com>
2019-12-04 23:19:49 -0300

    Aula 29 - Correção SweetAlert2

--- ControleEstoque.Web/Controllers/Cadastro/CadGrupoProdutoController.cs
+++ ControleEstoque.Web/Controllers/Cadastro/CadGrupoProdutoController.cs
@@ -40,8 +40,7 @@ namespace ControleEstoque.Web.Controllers
 		[ValidateAntiForgeryToken]
         public JsonResult RecuperarGrupoProduto(int id)
         {
-			throw new Exception();
-            return Json(GrupoProdutoModel.RecuperarPeloId(id));
+			return Json(GrupoProdutoModel.RecuperarPeloId(id));
         }
 
         [HttpPost]
--- ControleEstoque.Web/Scripts/Cadastro/CadBase.js
+++ ControleEstoque.Web/Scripts/Cadastro/CadBase.js
@@ -1,225 +1,235 @@
 ﻿function add_anti_forgery_token(data) {
-	data.__RequestVerificationToken = $('[name=__RequestVerificationToken]').val();
-	return data;
+    data.__RequestVerificationToken = $('[name=__RequestVerificationToken]').val();
+    return data;
 }
 
 function formatar_mensagem_aviso(mensagens) {
-	var ret = '';
+    var ret = '';
 
-	for (var i = 0; i < mensagens.length; i++) {
-		ret += '<li>' + mensagens[i] + '</li>';
-	}
+    for (var i = 0; i < mensagens.length; i++) {
+        ret += '<li>' + mensagens[i] + '</li>';
+    }
 
-	return '<ul>' + ret + '</ul>';
+    return '<ul>' + ret + '</ul>';
 }
 
 function abrir_form(dados) {
-	set_dados_form(dados);
+    set_dados_form(dados);
 
-	var modal_cadastro = $('#modal_cadastro');
+    var modal_cadastro = $('#modal_cadastro');
 
-	$('#msg_mensagem_aviso').empty();
-	$('#msg_aviso').hide();
-	$('#msg_mensagem_aviso').hide();
-	$('#msg_erro').hide();
+    $('#msg_mensagem_aviso').empty();
+    $('#msg_aviso').hide();
+    $('#msg_mensagem_aviso').hide();
+    $('#msg_erro').hide();
 
-	bootbox.dialog({
-		title: 'Cadastro de' + titulo_pagina,
-		message: modal_cadastro
-	})
-	.on('shown.bs.modal', function () {
-		modal_cadastro.show(0, function () {
-			set_focus_form();
-		});
-	})
-	.on('hidden.bs.modal', function () {
-		modal_cadastro.hide().appendTo('body');
-	});
+    bootbox.dialog({
+        title: 'Cadastro de' + titulo_pagina,
+        message: modal_cadastro
+    })
+        .on('shown.bs.modal', function () {
+            modal_cadastro.show(0, function () {
+                set_focus_form();
+            });
+        })
+        .on('hidden.bs.modal', function () {
+            modal_cadastro.hide().appendTo('body');
+        });
 }
 
 function criar_linha_grid(dados) {
-	var ret =
-		'<tr data-id=' + dados.Id + '>' +
-		set_dados_grid(dados) +
-		'<td>' +
-		'<a class="btn btn-primary btn-alterar" role="button" style="margin-right: 3px"><i class="glyphicon glyphicon-pencil"></i>Alterar</a>' +
-		'<a class="btn btn-danger btn-excluir" role="button"><i class="glyphicon glyphicon-trash"></i>Excluir</a>' +
-		'</td>' +
-		'</tr>';
-	
-	return ret;
+    var ret =
+        '<tr data-id=' + dados.Id + '>' +
+        set_dados_grid(dados) +
+        '<td>' +
+        '<a class="btn btn-primary btn-alterar" role="button" style="margin-right: 3px"><i class="glyphicon glyphicon-pencil"></i>Alterar</a>' +
+        '<a class="btn btn-danger btn-excluir" role="button"><i class="glyphicon glyphicon-trash"></i>Excluir</a>' +
+        '</td>' +
+        '</tr>';
+
+    return ret;
 }
 
 $(document).on('click', '#btn_incluir', function () {
-	abrir_form(get_dados_inclusao());
+    abrir_form(get_dados_inclusao());
 })
-.on('click', '.btn-alterar', function () {
-	var btn = $(this),
-		id = btn.closest('tr').attr('data-id'),
-		url = url_alterar,
-		param = { 'id': id };
-	$.post(url, add_anti_forgery_token(param), function (response) {
-		if (response) {
-			abrir_form(response);
-		}
-	})
-	.fail(function() {
-		swal('Aviso', 'Não foi possivel recuperar as informações. Tente novamente em instantes', 'warning');
-	});
-})
-.on('click', '.btn-excluir', function () {
-	var btn = $(this),
-		tr = btn.closest('tr'),
-		id = tr.attr('data-id'),
-		url = url_excluir,
-		param = { 'id': id };
+    .on('click', '.btn-alterar', function () {
+        var btn = $(this),
+            id = btn.closest('tr').attr('data-id'),
+            url = url_alterar,
+            param = { 'id': id };
+        $.post(url, add_anti_forgery_token(param), function (response) {
+            if (response) {
+                abrir_form(response);
+            }
+        })
+            .fail(function () {
+                Swal.fire('Aviso', 'Não foi possivel recuperar as informações. Tente novamente em instantes', 'warning');
+            });
+    })
+    .on('click', '.btn-excluir', function () {
+        var btn = $(this),
+            tr = btn.closest('tr'),
+            id = tr.attr('data-id'),
+            url = url_excluir,
+            param = { 'id': id };
 
-	bootbox.confirm({
-		message: "Realmente deseja excluir o " + titulo_pagina + "?",
-		buttons: {
-			confirm: {
-				label: 'Sim',
-				className: 'btn-danger'
-			},
-			cancel: {
-				label: 'Não',
-				className: 'btn-success'
-			}
-		},
-		callback: function (result) {
-			if (result) {
-				$.post(url, add_anti_forgery_token(param), function (response) {
-					if (response) {
-						tr.remove();
-						var quant = $('#grid_cadastro > tbody > tr').length;
-						if (quant == 0) {
-							$('#grid_cadastro').addClass('invisivel');
-							$('#mensagem_grid').removeClass('invisivel');
-						}
-					}
-				});
-			}
-		}
-	});
-})
-.on('click', '#btn_confirmar', function () {
-	var btn = $(this),
-		url = url_confirmar,
-		param = get_dados_form();
-	$.post(url, add_anti_forgery_token(param), function (response) {
-		if (response.Resultado == "OK") {
-			if (param.Id == 0) {
-				param.Id = response.IdSalvo;
-				var table = $('#grid_cadastro').find('tbody'),
-					linha = criar_linha_grid(param);
-				table.append(linha);
-				$('#grid_cadastro').removeClass('invisivel');
-				$('#mensagem_grid').addClass('invisivel');
-			} else {
-				var linha = $('#grid_cadastro').find('tr[data-id=' + param.Id + ']').find('td');
-				preencher_linha_grid(param, linha);
-			}
+        bootbox.confirm({
+            message: "Realmente deseja excluir o " + titulo_pagina + "?",
+            buttons: {
+                confirm: {
+                    label: 'Sim',
+                    className: 'btn-danger'
+                },
+                cancel: {
+                    label: 'Não',
+                    className: 'btn-success'
+                }
+            },
+            callback: function (result) {
+                if (result) {
+                    $.post(url, add_anti_forgery_token(param), function (response) {
+                        if (response) {
+                            tr.remove();
+                            var quant = $('#grid_cadastro > tbody > tr').length;
+                            if (quant == 0) {
+                                $('#grid_cadastro').addClass('invisivel');
+                                $('#mensagem_grid').removeClass('invisivel');
+                            }
+                        }
+                    }).fail(function () {
+                        Swal.fire('Aviso', 'Não foi possivel excluir as informações. Tente novamente em instantes', 'warning');
+                    });
+                }
+            }
+        });
+    })
+    .on('click', '#btn_confirmar', function () {
+        var btn = $(this),
+            url = url_confirmar,
+            param = get_dados_form();
+        $.post(url, add_anti_forgery_token(param), function (response) {
+            if (response.Resultado == "OK") {
+                if (param.Id == 0) {
+                    param.Id = response.IdSalvo;
+                    var table = $('#grid_cadastro').find('tbody'),
+                        linha = criar_linha_grid(param);
+                    table.append(linha);
+                    $('#grid_cadastro').removeClass('invisivel');
+                    $('#mensagem_grid').addClass('invisivel');
+                } else {
+                    var linha = $('#grid_cadastro').find('tr[data-id=' + param.Id + ']').find('td');
+                    preencher_linha_grid(param, linha);
+                }
 
-			$('#modal_cadastro').parents('.bootbox').modal('hide');
-		} else if (response.Resultado == "ERRO") {
-			$('#msg_aviso').hide();
-			$('#msg_mensagem_aviso').hide();
-			$('#msg_erro').show();
-		} else if (response.Resultado == "AVISO") {
-			$('#msg_mensagem_aviso').html(formatar_mensagem_aviso(response.Mensagens));
-			$('#msg_aviso').show();
-			$('#msg_mensagem_aviso').show();
-			$('#msg_erro').hide();
-		}
-	});
-})
-.on('click', '.page-item', function () {
-	var btn = $(this),
-		filtro = $('#txt_filtro'),
-		tamPag = $('#ddl_tam_pag').val(),
-		pagina = btn.text(),
-		url = url_page_click,
-		param = { 'pagina': pagina, 'tamPag': tamPag, 'filtro': filtro.val() };
+                $('#modal_cadastro').parents('.bootbox').modal('hide');
+            } else if (response.Resultado == "ERRO") {
+                $('#msg_aviso').hide();
+                $('#msg_mensagem_aviso').hide();
+                $('#msg_erro').show();
+            } else if (response.Resultado == "AVISO") {
+                $('#msg_mensagem_aviso').html(formatar_mensagem_aviso(response.Mensagens));
+                $('#msg_aviso').show();
+                $('#msg_mensagem_aviso').show();
+                $('#msg_erro').hide();
+            }
+        }).fail(function () {
+            Swal.fire('Aviso', 'Não foi possivel salvar. Tente novamente em instantes', 'warning');
+        });
+    })
+    .on('click', '.page-item', function () {
+        var btn = $(this),
+            filtro = $('#txt_filtro'),
+            tamPag = $('#ddl_tam_pag').val(),
+            pagina = btn.text(),
+            url = url_page_click,
+            param = { 'pagina': pagina, 'tamPag': tamPag, 'filtro': filtro.val() };
 
-	$.post(url, add_anti_forgery_token(param), function (response) {
-		if (response) {
-			var table = $('#grid_cadastro').find('tbody');
+        $.post(url, add_anti_forgery_token(param), function (response) {
+            if (response) {
+                var table = $('#grid_cadastro').find('tbody');
 
-			table.empty();
-			if (response.length > 0) {
-				$('#grid_cadastro').removeClass('invisivel');
-				$('#mensagem_grid').addClass('invisivel');
+                table.empty();
+                if (response.length > 0) {
+                    $('#grid_cadastro').removeClass('invisivel');
+                    $('#mensagem_grid').addClass('invisivel');
 
-				for (var i = 0; i < response.length; i++) {
-					table.append(criar_linha_grid(response[i]));
-				}
-			} else {
-				$('#grid_cadastro').addClass('invisivel');
-				$('#mensagem_grid').removeClass('invisivel');
-			}
+                    for (var i = 0; i < response.length; i++) {
+                        table.append(criar_linha_grid(response[i]));
+                    }
+                } else {
+                    $('#grid_cadastro').addClass('invisivel');
+                    $('#mensagem_grid').removeClass('invisivel');
+                }
 
-			btn.siblings().removeClass('active');
-			btn.addClass('active');
-		}
-	});
-})
-.on('change', '#ddl_tam_pag', function () {
-	var ddl = $(this),
-		filtro = $('#txt_filtro'),
-		tamPag = ddl.val(),
-		pagina = 1,
-		url = url_tam_page_change,
-		param = { 'pagina': pagina, 'tamPag': tamPag, 'filtro' : filtro.val()  };
+                btn.siblings().removeClass('active');
+                btn.addClass('active');
+            }
+        }).fail(function () {
+            Swal.fire('Aviso', 'Não foi possivel recuperar as informações. Tente novamente em instantes', 'warning');
+        });
+    })
+    .on('change', '#ddl_tam_pag', function () {
+        var ddl = $(this),
+            filtro = $('#txt_filtro'),
+            tamPag = ddl.val(),
+            pagina = 1,
+            url = url_tam_page_change,
+            param = { 'pagina': pagina, 'tamPag': tamPag, 'filtro': filtro.val() };
 
-	$.post(url, add_anti_forgery_token(param), function (response) {
-		if (response) {
-			var table = $('#grid_cadastro').find('tbody');
+        $.post(url, add_anti_forgery_token(param), function (response) {
+            if (response) {
+                var table = $('#grid_cadastro').find('tbody');
 
-			table.empty();
-			if (response.length > 0) {
-				$('#grid_cadastro').removeClass('invisivel');
-				$('#mensagem_grid').addClass('invisivel');
+                table.empty();
+                if (response.length > 0) {
+                    $('#grid_cadastro').removeClass('invisivel');
+                    $('#mensagem_grid').addClass('invisivel');
 
-				for (var i = 0; i < response.length; i++) {
-					table.append(criar_linha_grid(response[i]));
-				}
-			} else {
-				$('#grid_cadastro').addClass('invisivel');
-				$('#mensagem_grid').removeClass('invisivel');
-			}
+                    for (var i = 0; i < response.length; i++) {
+                        table.append(criar_linha_grid(response[i]));
+                    }
+                } else {
+                    $('#grid_cadastro').addClass('invisivel');
+                    $('#mensagem_grid').removeClass('invisivel');
+                }
 
-			ddl.siblings().removeClass('active');
-			ddl.addClass('active');
-		}
-	});
-})
-.on('keyup', '#txt_filtro', function () {
-	var filtro = $(this),
-		ddl = $('#ddl_tam_pag'),
-		tamPag = ddl.val(),
-		pagina = 1,
-		url = url_filtro_change,
-		param = { 'pagina': pagina, 'tamPag': tamPag, 'filtro' : filtro.val() };
+                ddl.siblings().removeClass('active');
+                ddl.addClass('active');
+            }
+        }).fail(function () {
+            Swal.fire('Aviso', 'Não foi possivel recuperar as informações. Tente novamente em instantes', 'warning');
+        });
+    })
+    .on('keyup', '#txt_filtro', function () {
+        var filtro = $(this),
+            ddl = $('#ddl_tam_pag'),
+            tamPag = ddl.val(),
+            pagina = 1,
+            url = url_filtro_change,
+            param = { 'pagina': pagina, 'tamPag': tamPag, 'filtro': filtro.val() };
 
-	$.post(url, add_anti_forgery_token(param), function (response) {
-		if (response) {
-			var table = $('#grid_cadastro').find('tbody');
+        $.post(url, add_anti_forgery_token(param), function (response) {
+            if (response) {
+                var table = $('#grid_cadastro').find('tbody');
 
-			table.empty();
-			if (response.length > 0) {
-				$('#grid_cadastro').removeClass('invisivel');
-				$('#mensagem_grid').addClass('invisivel');
+                table.empty();
+                if (response.length > 0) {
+                    $('#grid_cadastro').removeClass('invisivel');
+                    $('#mensagem_grid').addClass('invisivel');
 
-				for (var i = 0; i < response.length; i++) {
-					table.append(criar_linha_grid(response[i]));
-				}
-			} else {
-				$('#grid_cadastro').addClass('invisivel');
-				$('#mensagem_grid').removeClass('invisivel');
-			}
-			ddl.siblings().removeClass('active');
-			ddl.addClass('active');
-		}
-	});
-});
+                    for (var i = 0; i < response.length; i++) {
+                        table.append(criar_linha_grid(response[i]));
+                    }
+                } else {
+                    $('#grid_cadastro').addClass('invisivel');
+                    $('#mensagem_grid').removeClass('invisivel');
+                }
+                ddl.siblings().removeClass('active');
+                ddl.addClass('active');
+            }
+        }).fail(function () {
+            Swal.fire('Aviso', 'Não foi possivel recuperar as informações. Tente novamente em instantes', 'warning');
+        });;
+    });
 
